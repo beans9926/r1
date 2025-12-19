abe8c1acce5969576d25c0464e3313d87717e7c7
beans9926 <gulhanesarthak03@gmail.com>
2025-12-19 10:42:25 +0530

    cleanup

--- src/playerTriangle.cpp
+++ src/playerTriangle.cpp
@@ -11,22 +11,6 @@ void PlayerTriangle::updatePlayerPos(PlayerTriangle *player, int *screenWidth,
   float dx = mousePosition.x - player->p1.x;
   float dy = mousePosition.y - player->p1.y;
   player->rotation = atan2f(dy, dx) * RAD2DEG + 90.0f;
-
-  // player->len_p1_p3 = sqrt(std::pow((player->p3.x - player->p1.x), 2) +
-  //                          std::pow((player->p3.y - player->p1.y), 2));
-  // player->k_len_p1_p3 = 50 / player->len_p1_p3;
-  // player->p2 = {
-  //     (player->p1.x + (player->k_len_p1_p3 * (player->p3.x - player->p1.x))),
-  //     (player->p1.y + (player->k_len_p1_p3 * (player->p3.y -
-  //     player->p1.y)))};
-  // player->deltaX = player->p2.x - player->p1.x;
-  // player->deltaY = player->p2.y - player->p1.y;
-
-  // Rectangle playerTexRenderBounds = {player->p2.x, player->p2.y, 50, 100};
-
-  // Vector2 v0 = {(float)*screenWidth, (float)*screenHeight};
-  //  Vector2 v1Normal = Vector2Normalize(Vector2Subtract(player->p1, v0));
-  //  Vector2 v2Normal = Vector2Normalize(Vector2Subtract(player->p2, v0));
 }
 
 void PlayerTriangle::DrawPlayer(Texture2D *playerTexture,
